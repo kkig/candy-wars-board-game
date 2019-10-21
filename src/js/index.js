4aba779e4ea915cae306c3ccc76b4ae603df0861
kkig <kigoshi.kanazawa@gmail.com>
2019-10-21 17:08:15 +0200

    generate charactors

--- src/js/index.js
+++ src/js/index.js
@@ -1,73 +1,65 @@
 /* Select DOM element */
 const cell = document.getElementsByClassName('cell');
 
+
 /* Create array of cells */
 let cells = [...cell];
 let cellsArray = [];
+
+// Device array by row
 for (let i = 0; i + 10 <= cells.length; i += 10) {
     cellsArray.push(cells.slice(i, i + 10));
 }
+
 
 /* Generate Charactor */
 const playerOneImage = 'img/player/player_gr.png';
 const playerTwoImage = 'img/player/player_yr.png';
 
-let playerOneCell;
-let playerTwoCell;
-
-// Make deep copy of arrays
-//let newCellMap = JSON.parse(JSON.stringify(cellsArray));
-
-//let newCellMap = [...cellsArray];
-//let newCellMap = Array.from(cellsArray);
-
-const pickCells = () => {
-    
-
-    const setPlayerCell = array => {
-        const row = Math.floor(Math.random() * 9);
-        const column = Math.floor(Math.random() * 10);
-        
-        console.log('Row: ' + row + ' Col: ' + column);
-        //newCellMap[row][column] = null;
-        //console.log(newCellMap);
-        //console.log(cellsArray);
-        return array[row][column];
+class MapIcon {
+    constructor(row, column) {
+        this.row = row;
+        this.column = column;
     }
-
-    // Select Player One Cell
-    playerOneCell = setPlayerCell(cellsArray);
-    generatePlayer(playerOneImage, playerOneCell);
-    
-    // Select Player Two Cell
-    playerTwoCell = setPlayerCell(cellsArray);
-    /*
-    while(playerTwoCell = playerOneCell) {
-        playerTwoCell = setPlayerCell(cellsArray);
-    }
-    */
-
-    generatePlayer(playerTwoImage, playerTwoCell);
-
-
-    
-    /*
-    // Select Player Two Cell
-    let isCellNull = setPlayerCell(newCellMap);
-    while(isCellNull = null) {
-        setPlayerCell(newCellMap, playerTwoCell);
-    }
-    generatePlayer(playerTwoImage, playerTwoCell);    
-    */    
-};
-
-
+}
 
 const generatePlayer = (image, place) => {
     const playerImage = document.createElement('img');
     playerImage.setAttribute('src', image);
     place.append(playerImage);
 };
+
+const pickCell = () => {
+    const row = Math.floor(Math.random() * 9);
+    const column = Math.floor(Math.random() * 10);
+    
+    console.log('Row: ' + row + ' Col: ' + column);
+    return new MapIcon(row, column);
+}
+
+const generateIcons = () => {
+    let playerOne = pickCell();
+    let playerTwo = pickCell();
+
+    const sameCell = playerTwo.row == playerOne.row && playerTwo.column == playerOne.column;
+    //const cellsTouchXUp = playerTwo.column == playerOne.column && parseInt(playerTwo.row) - parseInt(playerOne.row) == 1;
+    //const cellsTouchXDown = playerTwo.column == playerOne.column && parseInt(playerTwo.row) - parseInt(playerOne.row) == -1;
+
+    //console.log(parseInt(playerTwo.row));
+    /*
+    while(sameCell) {
+        playerTwo = pickCell();
+    }
+    */
+
+    const generateCharactors = () => {
+        generatePlayer(playerOneImage, cellsArray[playerOne.row][playerOne.column]);
+        generatePlayer(playerTwoImage, cellsArray[playerTwo.row][playerTwo.column]);
+    };
+
+    generateCharactors();
+};
+
 
 
 
@@ -80,5 +72,5 @@ $('.start-button').on('click', function(e) {
         cell.innerHTML = '';
     }
 
-    pickCells();
+    generateIcons();
 });
